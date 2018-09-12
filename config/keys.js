42006edf33fbfaa928a5ad6c66df65498c7dac19
JSinnovation <tekipaq@gmail.com>
2018-09-12 16:19:24 -0500

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+ module.exports = {
+  mongoURI:"mongodb://Yaakov:Yaakov11@ds149252.mlab.com:49252/skyhawkconnector",
+  secretOrKey:"somethingsecret"
+}; 
+/* module.exports = {
+  mongoURI: "mongodb://localhost:27017/devconnector",
+  secretOrKey: "somethingsecret"
+};
+  */
