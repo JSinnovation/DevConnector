d92c863abd6c04b4a402f70d9f44c67ba0ef016b
JSinnovation <tekipaq@gmail.com>
2018-09-12 17:39:21 -0500

    update

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
- module.exports = {
+ /* module.exports = {
   mongoURI:"mongodb://Yaakov:Yaakov11@ds149252.mlab.com:49252/skyhawkconnector",
   secretOrKey:"somethingsecret"
-}; 
+};  */
 /* module.exports = {
   mongoURI: "mongodb://localhost:27017/devconnector",
   secretOrKey: "somethingsecret"
--- server.js
+++ server.js
@@ -2,28 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
 const app = express();
-
+mongoose.Promise = global.Promise; 
+mongoose.connect(process.env.MONGODB);
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(session({
+  secret: process.env.SECRET,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 // DB Config
-const db = require('./config/keys').mongoURI;
+//const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose
-.connect(
-  db,
-  { useNewUrlParser: true } //need this for api support
-)
-.then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+//mongoose.connect(process.env.MONGODB);
+//mongoose.Promise = global.Promise; 
+//mongoose.connect(process.env.MONGODB);
+ // db,
+  //{ useNewUrlParser: true } //need this for api support
+
+//.then(() => console.log('MongoDB Connected'))
+ // .catch(err => console.log(err))
 
 // Passport middleware
 app.use(passport.initialize());
