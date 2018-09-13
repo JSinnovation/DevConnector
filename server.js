0917e13ecc3ed4f4a57d4ac553a24360afc97703
JSinnovation <tekipaq@gmail.com>
2018-09-13 11:24:02 -0500

    updated env secret

--- server.js
+++ server.js
@@ -9,26 +9,17 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
 mongoose.Promise = global.Promise; 
+
 mongoose.connect(process.env.MONGODB);
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
   secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
-// DB Config
-//const db = require('./config/keys').mongoURI;
-
-// Connect to MongoDB
-//mongoose.connect(process.env.MONGODB);
-//mongoose.Promise = global.Promise; 
-//mongoose.connect(process.env.MONGODB);
- // db,
-  //{ useNewUrlParser: true } //need this for api support
-
-//.then(() => console.log('MongoDB Connected'))
- // .catch(err => console.log(err))
 
 // Passport middleware
 app.use(passport.initialize());
@@ -42,6 +33,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`)); 
