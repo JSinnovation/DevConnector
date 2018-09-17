e8f2b9e4bab263bda1ab600ff11472b6ec1fa3a8
JSinnovation <tekipaq@gmail.com>
2018-09-17 11:26:15 -0500

    updated per other project

--- server.js
+++ server.js
@@ -2,24 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
-mongoose.Promise = global.Promise; 
+mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true
-} );
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
-  store: new MongoStore({mongooseConnection: mongoose.connection})
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
 }));
 
 // Passport middleware
@@ -32,8 +41,10 @@ require('./config/passport')(passport);
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-app.use('/api/posts', posts); 
+app.use('/api/posts', posts);
 
-const port = process.env.PORT || 3000;
+const port = (process.env.PORT || 3000, ()=>{
+  console.log('Server running on port 3000');
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`)); 
+app.listen(port, () => console.log(`Server running on port ${port}`));
