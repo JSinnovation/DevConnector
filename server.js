5fe451aca941993a5f9fff3a529ed428d713f84f
JSinnovation <tekipaq@gmail.com>
2018-09-17 11:56:54 -0500

    test

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
 mongoose.Promise = global.Promise;
+var uristring =
+  process.env.MONGODB ||
+  'mongodb://localhost/HelloMongoose';
 
-mongoose
+/* mongoose
   .connect(process.env.MONGODB, {
     useNewUrlParser: true
-
       .then(() => console.log('MongoDB Connected'))
-      .catch(err => console.log(err))
-  })
+      .catch(err => console.log(err.extended))
+  }); */
 // Body parser middleware
 app.use(bodyParser.urlencoded({
   extended: false
@@ -44,8 +46,23 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = (process.env.PORT || 3000, () => {
-  console.log('Server running on port 3000');
+// Preamble
+var http = require('http'); // For serving a basic web page.
+var mongoose = require("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring =
+  process.env.MONGODB_URI ||
+  'mongodb://localhost/HelloMongoose';
+theport = (process.env.PORT || 3000);
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log('Succeeded connected to: ' + uristring);
+  }
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+//app.listen(port, () => console.log(`Server running on port ${port}`));
